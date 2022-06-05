6eb78cea6c43cc9294d143e8fc8a1c267f0ce9a4
eroyee <ivanmarcus@yahoo.com>
2022-06-05 12:05:36 +1200

    Auto-detect fft width from main code
    Canvas widths need to be in sync for spectrum display to coincide with waterfall. This had meant (re)hard-coding this code if/whenever fft size was changed in openwebrx specs, however these changes (and in openwebrx.js) mean this should auto-adjust without the need to hard code.

--- htdocs/lib/spectrum.js
+++ htdocs/lib/spectrum.js
@@ -26,93 +26,92 @@
     var freqSpectrumCtx;
     var freqSpectrumGradient; 
     var is_on;
-    var w = 2;
-    var w=localStorage.getItem("fft_size");
+    var fft_size=localStorage.getItem("fft_size"); // we first set fft_size into the browser from openwebrx.js, then collect it here to use with the spectrum canvas width (otherwise need to manually set width to 4096/8192/16384)
     function display_spectra () {
-	console.log (w);
-       if (!is_on) {
-       // Create a new DIV for spectrum analyzer canvas (in header.include.html)
-    var divFreqSpectrum = '<div id="freq-div-spectrum">'
-                        + '<canvas id="freq-canvas-spectrum" width="16384" height="256" style="width:100%;height:256px;left:0px;position:absolute;bottom:0px;">'
-                        + '</div>';
+    //console.log (w);
+    if (!is_on) {
+    // Create a new DIV for spectrum analyzer canvas (in header.include.html)
+        var divFreqSpectrum = '<div id="freq-div-spectrum">'
+                            + '<canvas id="freq-canvas-spectrum" width="fft_size" height="256" style="width:100%;height:256px;left:0px;position:absolute;bottom:0px;">'
+                            + '</div>';
 
-//eroyee below to drop down spectrum container
-    document.getElementById('spectrum_container').style.height = "130px";
-    document.getElementById('spectrum_container').style.opacity = "1";
-    var div = qs(".openwebrx-spectrum-container");
-    div.insertAdjacentHTML('beforeEnd', divFreqSpectrum);
+        //eroyee below to drop down spectrum container
+        document.getElementById('spectrum_container').style.height = "130px";
+        document.getElementById('spectrum_container').style.opacity = "1";
+        var div = qs(".openwebrx-spectrum-container");
+        div.insertAdjacentHTML('beforeEnd', divFreqSpectrum);
      
     // Canvas context for spectrum analyzer
     // var freqSpectrumCtx;
-    freqSpectrumCtx        = qs("#freq-canvas-spectrum").getContext('2d');
-    freqSpectrumCtx.width  = qs("#webrx-canvas-container").lastElementChild.width;
-    freqSpectrumCtx.height = 255
+        freqSpectrumCtx        = qs("#freq-canvas-spectrum").getContext('2d');
+        freqSpectrumCtx.width  = qs("#webrx-canvas-container").lastElementChild.width;
+        freqSpectrumCtx.height = 255
      
     // Gradient colorsceme for spectrum
     //var freqSpectrumGradient;   
-    freqSpectrumGradient = freqSpectrumCtx.createLinearGradient(0, 0, 0, 255);
-    freqSpectrumGradient.addColorStop(1.00, 'blue');
-    freqSpectrumGradient.addColorStop(0.75, 'green');
-    freqSpectrumGradient.addColorStop(0.50, 'yellow');
-    freqSpectrumGradient.addColorStop(0.25, 'orange');
-    freqSpectrumGradient.addColorStop(0.00, 'red');		   
+        freqSpectrumGradient = freqSpectrumCtx.createLinearGradient(0, 0, 0, 255);
+        freqSpectrumGradient.addColorStop(1.00, 'blue');
+        freqSpectrumGradient.addColorStop(0.75, 'green');
+        freqSpectrumGradient.addColorStop(0.50, 'yellow');
+        freqSpectrumGradient.addColorStop(0.25, 'orange');
+        freqSpectrumGradient.addColorStop(0.00, 'red');		   
     // Update spectrum analyzer
 	setInterval(function() {
             freqSpectrumDraw(getFreqData());
         }, 25) 	
-		// Observe zooming of waterfall and copy zoom-width to spectrum also
-    var observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutationRecord) {
-            qs("#freq-canvas-spectrum").style.left  = qs("#webrx-canvas-container").style.left
-            qs("#freq-canvas-spectrum").style.width = qs("#webrx-canvas-container").style.width
-        });    
-    });
-    observer.observe(qs("#webrx-canvas-container"), { attributes : true, attributeFilter : ['style'] });
-	is_on=true;	   
-	}
-// eroyee below to flush display & return up
-		else{
-		is_on=false;
+    // Observe zooming of waterfall and copy zoom-width to spectrum also
+        var observer = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutationRecord) {
+                qs("#freq-canvas-spectrum").style.left  = qs("#webrx-canvas-container").style.left
+                qs("#freq-canvas-spectrum").style.width = qs("#webrx-canvas-container").style.width
+            });    
+        });
+        observer.observe(qs("#webrx-canvas-container"), { attributes : true, attributeFilter : ['style'] });
+	    is_on=true;	   
+	    }
+    // eroyee below to flush display & return up
+    else{
+        is_on=false;
         document.getElementById('spectrum_container').style.height = "0px";
         document.getElementById('spectrum_container').style.opacity = "0";
         const flush = qs("#freq-canvas-spectrum");
         flush.parentNode.removeChild(flush);
-		return;
-		}
-// ----------------------------------------
-	}	
+        return;
+	}
+    // ----------------------------------------
+    }	
     // Function to draw spectrum
     function freqSpectrumDraw(freqData) {
-            // Clear spectrum canvasfunction display_spectra () {
-            freqSpectrumCtx.fillStyle = "#000";
-            freqSpectrumCtx.fillRect(0, 0, freqSpectrumCtx.width, freqSpectrumCtx.height);
-            freqSpectrumCtx.fillStyle = freqSpectrumGradient;  
-            // Draw spectrum
-            for (var i = 0; i < freqSpectrumCtx.width; i++) {
-                    freqSpectrumCtx.fillRect(i, freqSpectrumCtx.height, 1, -freqData[i]);
-            }
+    // Clear spectrum canvasfunction display_spectra () {
+        freqSpectrumCtx.fillStyle = "#000";
+        freqSpectrumCtx.fillRect(0, 0, freqSpectrumCtx.width, freqSpectrumCtx.height);
+        freqSpectrumCtx.fillStyle = freqSpectrumGradient;  
+    // Draw spectrum
+        for (var i = 0; i < freqSpectrumCtx.width; i++) {
+                freqSpectrumCtx.fillRect(i, freqSpectrumCtx.height, 1, -freqData[i]);
+        }
     }
      
     // Function to get data for spectrum, by grabbing the first row from waterfall canvas
     function getFreqData() {
-            // Get waterfall canvas
-            var theCanvas  = qs("#webrx-canvas-container").lastElementChild;
-            // Get canvas Y position
-            var theCanvasY = Math.abs(new WebKitCSSMatrix(window.getComputedStyle(theCanvas).webkitTransform).m42);
-            // Grab single row of data from that Y position
-            var theCanvasData = theCanvas.getContext('2d').getImageData(1, theCanvasY - 1, theCanvas.width, 1).data;
+    // Get waterfall canvas
+        var theCanvas  = qs("#webrx-canvas-container").lastElementChild;
+    // Get canvas Y position
+        var theCanvasY = Math.abs(new WebKitCSSMatrix(window.getComputedStyle(theCanvas).webkitTransform).m42);
+    // Grab single row of data from that Y position
+        var theCanvasData = theCanvas.getContext('2d').getImageData(1, theCanvasY - 1, theCanvas.width, 1).data;
      
-            // Convert row of pixels to spectrum analyser data
-            var data = [];
-            var x = 0;
-            for (var i = 0; i < theCanvasData.length; i += 4) {
-                    // RGB data to Grayscale
-                    var avg = (theCanvasData[i] + theCanvasData[i + 2] + theCanvasData[i + 1]) / 3;  // mess with these numbers to get desired output
-                    data[x++] = parseInt(avg) - 38;  // subtract number to lower floor hard level
-            }
-   		    return data;
-//		return;
-	}
+    // Convert row of pixels to spectrum analyser data
+        var data = [];
+        var x = 0;
+        for (var i = 0; i < theCanvasData.length; i += 4) {
+    // RGB data to Grayscale
+                var avg = (theCanvasData[i] + theCanvasData[i + 2] + theCanvasData[i + 1]) / 3;  // mess with these numbers to get desired output
+                data[x++] = parseInt(avg) - 38;  // subtract number to lower floor hard level
+        }
+        return data;
+//	return;
+    }
      
    
     
