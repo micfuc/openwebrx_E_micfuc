9754a526077476af4e970d3db1178bb0232e4948
eroyee <luke@lp>
2022-07-09 21:32:27 +1200

    Add further menu options

--- htdocs/lib/DemodulatorPanel.js
+++ htdocs/lib/DemodulatorPanel.js
@@ -43,66 +43,59 @@ function DemodulatorPanel(el) {
     window.addEventListener('hashchange', function () {
         self.onHashChange();
     });
-}
-;
+};
 
-DemodulatorPanel.prototype.render = function () {
-    var available = Modes.getModes().filter(function (m) {
-        return m.isAvailable();
-    });
-    var normalModes = available.filter(function (m) {
-        return m.type === 'analog';
-    });
-    var digiModes = available.filter(function (m) {
-        return m.type === 'digimode';
-    });
+DemodulatorPanel.prototype.render = function() {
+    var available = Modes.getModes().filter(function(m){ return m.isAvailable(); });
+    var normalModes = available.filter(function(m){ return m.type === 'analog'; });
+    var digiModes = available.filter(function(m){ return m.type === 'digimode'; });
 
     var html = []
 
-    var buttons = normalModes.map(function (m) {
+    var buttons = normalModes.map(function(m){
         return $(
-                '<div ' +
+            '<div ' +
                 'class="openwebrx-button openwebrx-demodulator-button" ' +
                 'data-modulation="' + m.modulation + '" ' +
                 'id="openwebrx-button-' + m.modulation + '" r' +
-                '>' + m.name + '</div>'
-                );
+            '>' + m.name + '</div>'
+        );
     });
 
     var $modegrid = $('<div class="openwebrx-modes-grid"></div>');
     $modegrid.append.apply($modegrid, buttons);
     html.push($modegrid);
-    /*
-     html.push($(
-     '<div class="openwebrx-panel-line openwebrx-panel-flex-line">' +
-     '<div class="openwebrx-button openwebrx-demodulator-button openwebrx-button-dig">DIG</div>' +
-     '<select class="openwebrx-secondary-demod-listbox">' +
-     '<option value="none"></option>' +
-     digiModes.map(function(m){
-     return '<option value="' + m.modulation + '">' + m.name + '</option>';
-     }).join('') +
-     '</select>' +
-     '</div>'
-     ));
-     
-     this.el.find(".openwebrx-modes").html(html);
-     };
-     */
+/*
+    html.push($(
+        '<div class="openwebrx-panel-line openwebrx-panel-flex-line">' +
+            '<div class="openwebrx-button openwebrx-demodulator-button openwebrx-button-dig">DIG</div>' +
+            '<select class="openwebrx-secondary-demod-listbox">' +
+                '<option value="none">Digital Modes Select</option>' +
+                digiModes.map(function(m){
+                    return '<option value="' + m.modulation + '">' + m.name + '</option>';
+                }).join('') +
+            '</select>' +
+        '</div>'
+    ));
+
+    this.el.find(".openwebrx-modes").html(html);
+};
+*/
 
     html.push($(
             '<div class="openwebrx-panel-line openwebrx-panel-flex-line">' +
             '<select class="openwebrx-secondary-demod-listbox">' +
-            '<option value="none">Digital Modes:</option>' +
-            digiModes.map(function (m) {
-                return '<option value="' + m.modulation + '">' + m.name + '</option>';
-            }).join('') +
+                '<option value="none">Digital Modes:</option>' +
+                digiModes.map(function(m){
+                    return '<option value="' + m.modulation + '">' + m.name + '</option>';
+                }).join('') +
             '</select>' +
             '<select class="openwebrx-secondary-demod-listbox" id="change_display">' +
-            '<option value="none">Displays:</option>' + '<option value="1">Timeseries</option>' + '<option value="2">Spectrum Line</option>' + '<option value="3">Spectrum Fill</option>' + '<option value="4">Waterfall Toggle</option>' +
-            '</select>' +
+                '<option value="none">Displays:</option>'+'<option value="1">Timeseries</option>'+'<option value="2">Timeseries Tick</option>'+'<option value="3">Spectrum Line</option>'+ '<option value="4">Spectrum Fill</option>'+'<option value="5">Waterfall Off</option>'+'<option value="6">Waterfall On</option>'+
+            '</select>'+ 
             '<button id="peak" class="openwebrx-SpectraHold-button" onclick="peak_spectra_hold()" style="margin-left:1px" title="Spectrum peak hold on/off" >^</button>' +
-            '</div>'
-            ));
+        '</div>'
+    ));
     this.el.find(".openwebrx-modes").html(html);
 };
 
@@ -142,16 +135,16 @@ DemodulatorPanel.prototype.setMode = function (requestedModulation) {
     this.demodulator.setSquelch(current.squelch_level);
 
     var self = this;
-    var updateFrequency = function (freq) {
+    var updateFrequency = function(freq) {
         self.tuneableFrequencyDisplay.setFrequency(self.center_freq + freq);
         self.updateHash();
     };
     this.demodulator.on("frequencychange", updateFrequency);
     updateFrequency(this.demodulator.get_offset_frequency());
-    var updateSquelch = function (squelch) {
+    var updateSquelch = function(squelch) {
         self.el.find('.openwebrx-squelch-slider')
-                .val(squelch)
-                .attr('title', 'Squelch (' + squelch + ' dB)');
+            .val(squelch)
+            .attr('title', 'Squelch (' + squelch + ' dB)');
         self.updateHash();
     };
     this.demodulator.on('squelchchange', updateSquelch);
@@ -174,13 +167,13 @@ DemodulatorPanel.prototype.setMode = function (requestedModulation) {
     this.updateHash();
 };
 
-DemodulatorPanel.prototype.disableDigiMode = function () {
+DemodulatorPanel.prototype.disableDigiMode = function() {
     // just a little trick to get out of the digimode
     delete this.mode;
     this.setMode(this.getDemodulator().get_modulation());
 };
 
-DemodulatorPanel.prototype.updatePanels = function () {
+DemodulatorPanel.prototype.updatePanels = function() {
     var modulation = this.getDemodulator().get_secondary_demod();
     $('#openwebrx-panel-digimodes').attr('data-mode', modulation);
     toggle_panel("openwebrx-panel-digimodes", !!modulation);
@@ -195,16 +188,16 @@ DemodulatorPanel.prototype.updatePanels = function () {
     metaPanels.each(function (_, p) {
         toggle_panel(p.id, p.id === showing);
     });
-    metaPanels.metaPanel().each(function () {
+    metaPanels.metaPanel().each(function() {
         this.clear();
     });
 };
 
-DemodulatorPanel.prototype.getDemodulator = function () {
+DemodulatorPanel.prototype.getDemodulator = function() {
     return this.demodulator;
 };
 
-DemodulatorPanel.prototype.collectParams = function () {
+DemodulatorPanel.prototype.collectParams = function() {
     var defaults = {
         offset_frequency: 0,
         squelch_level: -150,
@@ -213,14 +206,13 @@ DemodulatorPanel.prototype.collectParams = function () {
     return $.extend(new Object(), defaults, this.validateInitialParams(this.initialParams), this.transformHashParams(this.parseHash()));
 };
 
-DemodulatorPanel.prototype.startDemodulator = function () {
-    if (!Modes.initComplete() || !this.center_freq)
-        return;
+DemodulatorPanel.prototype.startDemodulator = function() {
+    if (!Modes.initComplete() || !this.center_freq) return;
     var params = this.collectParams();
     this._apply(params);
 };
 
-DemodulatorPanel.prototype.stopDemodulator = function () {
+DemodulatorPanel.prototype.stopDemodulator = function() {
     if (!this.demodulator) {
         return;
     }
@@ -229,33 +221,31 @@ DemodulatorPanel.prototype.stopDemodulator = function () {
     this.mode = null;
 }
 
-DemodulatorPanel.prototype._apply = function (params) {
+DemodulatorPanel.prototype._apply = function(params) {
     this.setMode(params.mod);
     this.getDemodulator().set_offset_frequency(params.offset_frequency);
     this.getDemodulator().setSquelch(params.squelch_level);
     this.updateButtons();
 };
 
-DemodulatorPanel.prototype.setInitialParams = function (params) {
+DemodulatorPanel.prototype.setInitialParams = function(params) {
     $.extend(this.initialParams, params);
 };
 
-DemodulatorPanel.prototype.resetInitialParams = function () {
+DemodulatorPanel.prototype.resetInitialParams = function() {
     this.initialParams = {};
 };
 
-DemodulatorPanel.prototype.onHashChange = function () {
+DemodulatorPanel.prototype.onHashChange = function() {
     this._apply(this.transformHashParams(this.parseHash()));
 };
 
-DemodulatorPanel.prototype.transformHashParams = function (params) {
+DemodulatorPanel.prototype.transformHashParams = function(params) {
     var ret = {
         mod: params.secondary_mod || params.mod
     };
-    if (typeof (params.offset_frequency) !== 'undefined')
-        ret.offset_frequency = params.offset_frequency;
-    if (typeof (params.sql) !== 'undefined')
-        ret.squelch_level = parseInt(params.sql);
+    if (typeof(params.offset_frequency) !== 'undefined') ret.offset_frequency = params.offset_frequency;
+    if (typeof(params.sql) !== 'undefined') ret.squelch_level = parseInt(params.sql);
     return ret;
 };
 
@@ -263,12 +253,11 @@ DemodulatorPanel.prototype.squelchAvailable = function () {
     return this.mode && this.mode.squelch;
 }
 
-DemodulatorPanel.prototype.updateButtons = function () {
+DemodulatorPanel.prototype.updateButtons = function() {
     var $buttons = this.el.find(".openwebrx-demodulator-button");
     $buttons.removeClass("highlighted").removeClass('same-mod');
     var demod = this.getDemodulator()
-    if (!demod)
-        return;
+    if (!demod) return;
     this.el.find('[data-modulation=' + demod.get_modulation() + ']').addClass("highlighted");
     var secondary_demod = demod.get_secondary_demod()
     if (secondary_demod) {
@@ -277,9 +266,9 @@ DemodulatorPanel.prototype.updateButtons = function () {
         var mode = Modes.findByModulation(secondary_demod);
         if (mode) {
             var self = this;
-            mode.underlying.filter(function (m) {
+            mode.underlying.filter(function(m) {
                 return m !== demod.get_modulation();
-            }).forEach(function (m) {
+            }).forEach(function(m) {
                 self.el.find('[data-modulation=' + m + ']').addClass('same-mod')
             });
         }
@@ -291,13 +280,13 @@ DemodulatorPanel.prototype.updateButtons = function () {
     this.el.find('.openwebrx-squelch-auto')[squelch_disabled ? 'addClass' : 'removeClass']('disabled');
 }
 
-DemodulatorPanel.prototype.setCenterFrequency = function (center_freq) {
+DemodulatorPanel.prototype.setCenterFrequency = function(center_freq) {
     var me = this;
     if (me.centerFreqTimeout) {
         clearTimeout(me.centerFreqTimeout);
         me.centerFreqTimeout = false;
     }
-    this.centerFreqTimeout = setTimeout(function () {
+    this.centerFreqTimeout = setTimeout(function() {
         me.stopDemodulator();
         me.center_freq = center_freq;
         me.startDemodulator();
@@ -305,14 +294,14 @@ DemodulatorPanel.prototype.setCenterFrequency = function (center_freq) {
     }, 50);
 };
 
-DemodulatorPanel.prototype.parseHash = function () {
+DemodulatorPanel.prototype.parseHash = function() {
     if (!window.location.hash) {
         return {};
     }
-    var params = window.location.hash.substring(1).split(",").map(function (x) {
+    var params = window.location.hash.substring(1).split(",").map(function(x) {
         var harr = x.split('=');
         return [harr[0], harr.slice(1).join('=')];
-    }).reduce(function (params, p) {
+    }).reduce(function(params, p){
         params[p[0]] = p[1];
         return params;
     }, {});
@@ -320,14 +309,14 @@ DemodulatorPanel.prototype.parseHash = function () {
     return this.validateHash(params);
 };
 
-DemodulatorPanel.prototype.validateHash = function (params) {
+DemodulatorPanel.prototype.validateHash = function(params) {
     var self = this;
-    params = Object.keys(params).filter(function (key) {
+    params = Object.keys(params).filter(function(key) {
         if (key == 'freq' || key == 'mod' || key == 'secondary_mod' || key == 'sql') {
             return params.freq && Math.abs(params.freq - self.center_freq) <= bandwidth / 2;
         }
         return true;
-    }).reduce(function (p, key) {
+    }).reduce(function(p, key) {
         p[key] = params[key];
         return p;
     }, {});
@@ -340,66 +329,61 @@ DemodulatorPanel.prototype.validateHash = function (params) {
     return params;
 };
 
-DemodulatorPanel.prototype.validateInitialParams = function (params) {
+DemodulatorPanel.prototype.validateInitialParams = function(params) {
     return Object.fromEntries(
-            Object.entries(params).filter(function (a) {
-        if (a[0] == "offset_frequency") {
-            return Math.abs(a[1]) <= bandwidth / 2;
-        }
-        return true;
-    })
-            );
+        Object.entries(params).filter(function(a) {
+            if (a[0] == "offset_frequency") {
+                return Math.abs(a[1]) <= bandwidth / 2;
+            }
+            return true;
+        })
+    );
 };
 
-DemodulatorPanel.prototype.updateHash = function () {
+DemodulatorPanel.prototype.updateHash = function() {
     var demod = this.getDemodulator();
-    if (!demod)
-        return;
+    if (!demod) return;
     var self = this;
     window.location.hash = $.map({
         freq: demod.get_offset_frequency() + self.center_freq,
         mod: demod.get_modulation(),
         secondary_mod: demod.get_secondary_demod(),
         sql: demod.getSquelch(),
-    }, function (value, key) {
-        if (typeof (value) === 'undefined' || value === false)
-            return undefined;
+    }, function(value, key){
+        if (typeof(value) === 'undefined' || value === false) return undefined;
         return key + '=' + value;
-    }).filter(function (v) {
+    }).filter(function(v) {
         return !!v;
     }).join(',');
 };
 
-DemodulatorPanel.prototype.updateSquelch = function () {
+DemodulatorPanel.prototype.updateSquelch = function() {
     var sliderValue = parseInt(this.el.find(".openwebrx-squelch-slider").val());
     var demod = this.getDemodulator();
-    if (demod)
-        demod.setSquelch(sliderValue);
+    if (demod) demod.setSquelch(sliderValue);
 };
 
-DemodulatorPanel.prototype.setSquelchMargin = function (margin) {
-    if (typeof (margin) === 'undefined' || this.squelchMargin == margin)
-        return;
+DemodulatorPanel.prototype.setSquelchMargin = function(margin) {
+    if (typeof(margin) === 'undefined' || this.squelchMargin == margin) return;
     this.squelchMargin = margin;
 };
 
-DemodulatorPanel.prototype.getSquelchMargin = function () {
+DemodulatorPanel.prototype.getSquelchMargin = function() {
     return this.squelchMargin;
 };
 
-DemodulatorPanel.prototype.setMouseFrequency = function (freq) {
+DemodulatorPanel.prototype.setMouseFrequency = function(freq) {
     this.mouseFrequencyDisplay.setFrequency(freq);
 };
 
-DemodulatorPanel.prototype.setTuningPrecision = function (precision) {
+DemodulatorPanel.prototype.setTuningPrecision = function(precision) {
     this.tuneableFrequencyDisplay.setTuningPrecision(precision);
     this.mouseFrequencyDisplay.setTuningPrecision(precision);
 };
 
-$.fn.demodulatorPanel = function () {
+$.fn.demodulatorPanel = function(){
     if (!this.data('panel')) {
         this.data('panel', new DemodulatorPanel(this));
-    }
-    ;
+    };
     return this.data('panel');
 };
